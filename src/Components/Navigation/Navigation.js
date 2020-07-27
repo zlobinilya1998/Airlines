f88d34a68aeeefd9af8d3bda734bec7b902b30a5
zlobin_ilya <qwertin1998@gmail.com>
2020-07-28 00:26:12 +0300

    Upload

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -20,10 +20,6 @@ class Navigation extends React.Component{
                         <a className="nav-link" href="#">Маршруты</a>
                     </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
-                    </form>
                 </div>
             </nav>
             </>
