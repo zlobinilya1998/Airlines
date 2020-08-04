70deb31fc69dbd7b91d504539ebd0cacdfae0979
zlobin_ilya <qwertin1998@gmail.com>
2020-08-04 11:19:09 +0300

    up

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Navigation from './Components/Navigation/Navigation'
-import Test from './Components/Test/Test'
 
 
 
 ReactDOM.render(
   <>
     <Navigation/>
-    <Test/>
 
   </>,
   document.getElementById('root')
